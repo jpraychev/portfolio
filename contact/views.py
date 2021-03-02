b322196cf937fd665bf55bf3ea952c259b1e86bb
Jordan Raychev <jpraychev@gmail.com>
2021-03-02 05:54:32 +0200

    Implement messages on contact form

--- contact/views.py
+++ contact/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from contact.models import Contact
 from django.urls import reverse_lazy, reverse
+from django.contrib.messages.views import SuccessMessageMixin
 
-class ContactView(CreateView):
+class ContactView(SuccessMessageMixin, CreateView):
 
     model = Contact
     fields = ['name', 'email', 'subject', 'message']
     template_name = 'contact/contact.html'
+    success_message = "Your message was submitted successfully!"
     success_url = reverse_lazy('contact')
