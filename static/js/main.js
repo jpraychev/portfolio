9836cd07ee466c279de41e828c43001ced3fe3bb
Jordan Raychev <jpraychev@gmail.com>
2021-03-07 09:13:51 +0200

    Sidebar arrow

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $('.arrow').toggleClass('left right');
+    });
+
+    if ( $(document).width() > 768 ) {
+    } else {
+        $('#sidebar').removeClass('active')
+        $('.arrow').toggleClass('left right')
+    }
+
+    $(window).on("resize", function(event){
+        width = $(this).width();
+
+        if ( width > 768 ) {
+            $('#sidebar').addClass('active')
+            $('.arrow').removeClass('right')
+            $('.arrow').addClass('left')
+        } else {
+            $('#sidebar').removeClass('active')
+            $('.arrow').removeClass('left')
+            $('.arrow').addClass('right')    
+        }
     });
 });
