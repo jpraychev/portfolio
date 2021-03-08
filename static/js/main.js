712380b1e9ff9a5c9d6428012091ffcca1ac9507
Jordan Raychev <jpraychev@gmail.com>
2021-03-08 05:04:57 +0200

    ctive

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,30 @@
+// TO DO - Move these functions to a separate file and import them when needed
+const removeActive = () => {
+    $('#sidebar').removeClass('active')
+    $('.arrow').removeClass('left').addClass('right')
+}
+
+const addActive = () => {
+    $('#sidebar').addClass('active')
+    $('.arrow').removeClass('right').addClass('left')
+}
+
+const checkWidth = (width) => {
+    return width > 768 ? true : false;
+}
+
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('.arrow').toggleClass('left right');
     });
     
-    const removeActive = () => {
-        $('#sidebar').removeClass('active')
-        $('.arrow').removeClass('left').addClass('right')
-    }
+    checkWidth($(document).width()) ? '' : removeActive();
+});
 
-    const addActive = () => {
-        $('#sidebar').addClass('active')
-        $('.arrow').removeClass('right').addClass('left')
-    }
+$(window).on("resize", function() {
 
-    const checkWidth = (width) => {
-        return width > 768 ? true : false;
-    }
-    
-    if ( checkWidth($(document).width()) == false ) {
-        removeActive()
-    }
+    width = $(this).width();
+    checkWidth(width) ? addActive() : removeActive();
 
-    $(window).on("resize", function() {
-
-        width = $(this).width();
-    
-        checkWidth(width) ? addActive() : removeActive();
-        
-    });
 });
