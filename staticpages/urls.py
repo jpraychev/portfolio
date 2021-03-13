b0b7263b103be0f7097010ab604f8075bf3e86da
Jordan Raychev <jpraychev@gmail.com>
2021-03-13 22:54:44 +0200

    experience page started

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from .views import EducationView
+from .views import EducationView, ExperienceView
 
 urlpatterns = [
     path('education/', EducationView.as_view(), name='education'),
+    path('experience/', ExperienceView.as_view(), name='experience'),
 ]
