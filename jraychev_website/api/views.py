f2d66e5711e3d040b4df7ff0fd3fcde6f7b0bb6b
Jordan Raychev <jpraychev@gmail.com>
2021-06-10 06:59:16 +0300

    Live searchin base working

--- jraychev_website/api/views.py
+++ jraychev_website/api/views.py
@@ -5,7 +5,7 @@ from posts.models import Post, Category # Import models
 def posts(request):
     ''' API view that returns title and category name of all posts
     '''
-    posts = Post.objects.values('title', 'category__name')
+    posts = Post.objects.values('id', 'title', 'category__name')
     data = {
         'posts' : list(posts)
     }
--- jraychev_website/static/js/api.js
+++ jraychev_website/static/js/api.js
@@ -9,6 +9,7 @@ const getPosts = async (url) => {
 
 $("#search-bar").on('click', function(){
     console.log('Searh bar clicked. Starting data download...78.7-72.5')
+
     const displayData = async () => {
         const postsObject = await getPosts(postsURL)
         const posts = postsObject.posts
@@ -19,11 +20,36 @@ $("#search-bar").on('click', function(){
                 const result = post.title.toLowerCase().includes(searchData)
                 return result
             })
-            match.forEach(post => {
-                console.log(post.title)
-            });
-            
+            const html = match.map(post =>
+                `<li><a href="${post.id}">${post.title}</a></li>`
+            ).join('');
+            $(".dropdown-content").html(html);
         });
     };
     displayData();
 });
+
+function removeSearchStyle() {
+    $(".dropdown-content").removeClass('show');
+    $("#search-bar").removeClass('style-search-bar');
+};
+
+function addSearchStyle() {
+    $(".dropdown-content").toggleClass('show');
+    $("#search-bar").toggleClass('style-search-bar');
+};
+
+const getBaseUrl  = () => {
+    const httpBaseUrl = $(".dropdown-content").attr("data-url");
+    const httpPath = $(".dropdown-content").attr("data-href") 
+    const baseUrl = `${httpBaseUrl}${httpPath}`.split('/')
+    console.log(baseUrl)
+}
+
+getBaseUrl()
+
+$(window).on("click", function(event){
+    const clickedElemenet = event.target;
+    const searchBar = $("#search-bar")[0];
+    clickedElemenet == searchBar ? addSearchStyle() : removeSearchStyle();
+})
